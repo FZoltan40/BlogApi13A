58e2b0771eddc992fd71e20d1b0b11f610635d01
FZoltan40 <farkasz@kkszki.hu>
2025-11-21 08:47:35 +0100

    Blogger model is created and connection via blogger is ready.

--- BlogApi/Models/Blogger.cs
+++ BlogApi/Models/Blogger.cs
@@ -1,11 +1,25 @@
-﻿namespace BlogApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogApi.Models
 {
     public class Blogger
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(30)")]
         public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "text")]
+        public string? Password { get; set; }
+
+        [Column(TypeName = "text")]
         public string Email { get; set; }
         public DateTime RegTime { get; set; } = DateTime.Now;
         public DateTime ModTime { get; set; } = DateTime.Now;
+        public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
     }
 }
--- BlogApi/obj/Debug/net8.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/obj/Debug/net8.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4eed8be5344d7d7eb3dae42aef540284e940ed0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+697a565398efefadee88c773b8870ff3221aa6ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
